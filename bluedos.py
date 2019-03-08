9328ce588f5db4793c1f55656a23e0a180f4e521
otx2s <47149411+otx2s@users.noreply.github.com>
2019-03-08 16:06:12 +0700

    Update

--- bluedos.py
+++ bluedos.py
@@ -5,12 +5,8 @@ import subprocess
 from time import sleep as ts
 
 R = "\033[91m" 
-P = "\033[95m" 
-C = "\033[96m" 
-DC = "\033[36m" 
 B = "\033[94m" 
 G = "\033[92m" 
-Y = "\033[93m" 
 W = "\033[0m"
 BOLD = "\033[1m"
 POD = "\033[4m"
